1e1273a390446ebb5b59ab7e0637fc01ac0c4c88
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2023-04-27 00:51:10 +0300

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Bakhtiyorjon Mirzajonov on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
